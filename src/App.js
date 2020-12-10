639722c37c71fcd6c85551298c343809d18dda6d
KK44689 <kirima111.04@hotmail.com>
2020-12-10 17:00:59 +0700

    Profile and Education

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import classes from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import {Navbar,Nav} from 'react-bootstrap'; 
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap';
 import Home from './App.js';
 
 function App() {
@@ -17,6 +17,39 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      <Container fluid id={classes.container}>
+        <Row>
+          <Col md lg="6">
+
+          </Col>
+          <Col md lg="5" id={classes.profile}>
+            <h1 className={classes.profileTitle}>Profile</h1>
+            <p className={classes.profileDetails}>
+              Name : Kirima Kangwanwongsakul<br />
+              Age : 20<br />
+              Tel : 094-481-8112<br />      Line ID : seakirima<br />
+              E-mail : kirima111.04@gmail.com<br />
+              Address : 204  หมู่บ้านพงษ์ศิริชัย
+              ถนนบรมราชชนนี แขวงศาลาธรรมสพน์
+              เขตทวีวัฒนา กรุงเทพฯ  10170<br />
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col id={classes.education}>
+            <h1 className={classes.eduTitle}>Education</h1>
+            <p className={classes.eduDetails}>
+              - จบระดับชั้นมัธยมศึกษา จากโรงเรียนสตรีวิทยา ด้วยเกรดเฉลี่ย 3.54<br />
+              - กำลังศึกษาในระดับปริญญาตรี  สาขาคอมพิวเตอร์  คณะวิศวกรรมศาสตร์  มหาวิทยาลัยเกษตรศาสตร์ <br/>&nbsp;&nbsp;ด้วยเกรดเฉลี่ยสะสม  2.76
+            </p>
+          </Col>
+        </Row>
+        {/* <Row>
+          <Col>
+          ,</Col>
+        </Row> */}
+      </Container>
     </div>
   );
 }
