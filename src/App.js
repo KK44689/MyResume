8ef0059676270556a3679a50d1a181c0ac0c89ed
KK44689 <kirima111.04@hotmail.com>
2020-12-10 15:35:00 +0700

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import {Navbar,Nav} from 'react-bootstrap'; 
+import Home from './App.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" expand="lg" id={classes.Nav} >
+        <Navbar.Brand href="/Home" id={classes.navTitle}>Kirima Kangwanwongsakul's Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="./App.js" id={classes.navOption}>Online-Shopping page</Nav.Link>
+            <Nav.Link href="#link" id={classes.navOption}>Signup Form page</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
