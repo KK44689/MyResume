38302485020919eaeee1db008d68472fc60e4964
KK44689 <kirima111.04@hotmail.com>
2020-12-11 12:02:15 +0700

    Change layout + add profile image + add education + add skill

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './App.js';
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" expand="lg" id={classes.Nav} >
+      <Navbar fixed="top" expand="lg" id={classes.Nav} >
         <Navbar.Brand href="/Home" id={classes.navTitle}>Kirima Kangwanwongsakul's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -18,37 +18,70 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Container fluid id={classes.container}>
-        <Row>
+      <Container fluid>
+        <Row id={classes.profile}>
           <Col md lg="6">
-
+            <img className={classes.ProfilePic} src="/ProfilePic.jpg" />
           </Col>
-          <Col md lg="5" id={classes.profile}>
+          <Col md lg="5">
             <h1 className={classes.profileTitle}>Profile</h1>
             <p className={classes.profileDetails}>
-              Name : Kirima Kangwanwongsakul<br />
-              Age : 20<br />
-              Tel : 094-481-8112<br />      Line ID : seakirima<br />
-              E-mail : kirima111.04@gmail.com<br />
+              <br />
+              Name : Kirima Kangwanwongsakul<br /><br />
+              Age : 20<br /><br />
+              Tel : 094-481-8112<br /><br />      Line ID : seakirima<br /><br />
+              E-mail : kirima111.04@gmail.com<br /><br />
               Address : 204  หมู่บ้านพงษ์ศิริชัย
               ถนนบรมราชชนนี แขวงศาลาธรรมสพน์
-              เขตทวีวัฒนา กรุงเทพฯ  10170<br />
+              เขตทวีวัฒนา กรุงเทพฯ  10170<br /><br />
             </p>
           </Col>
         </Row>
-        <Row>
-          <Col id={classes.education}>
+        <Row id={classes.education}>
+          <Col >
             <h1 className={classes.eduTitle}>Education</h1>
             <p className={classes.eduDetails}>
-              - จบระดับชั้นมัธยมศึกษา จากโรงเรียนสตรีวิทยา ด้วยเกรดเฉลี่ย 3.54<br />
-              - กำลังศึกษาในระดับปริญญาตรี  สาขาคอมพิวเตอร์  คณะวิศวกรรมศาสตร์  มหาวิทยาลัยเกษตรศาสตร์ <br/>&nbsp;&nbsp;ด้วยเกรดเฉลี่ยสะสม  2.76
+              <br />
+              - จบระดับชั้นมัธยมศึกษา จากโรงเรียนสตรีวิทยา ด้วยเกรดเฉลี่ย 3.54<br /><br />
+              - กำลังศึกษาในระดับปริญญาตรี  สาขาคอมพิวเตอร์  คณะวิศวกรรมศาสตร์  มหาวิทยาลัยเกษตรศาสตร์ <br /><br />&nbsp;&nbsp;ด้วยเกรดเฉลี่ยสะสม  2.76
             </p>
           </Col>
         </Row>
-        {/* <Row>
+        <Row id={classes.experience}>
           <Col>
-          ,</Col>
-        </Row> */}
+            <h1 className={classes.expTitle}>Experience</h1>
+            <p className={classes.expDetails}>
+              <br />
+              <span>2562</span> &nbsp;&nbsp;ค่ายพัฒนาซอฟต์แวร์สภาพการทำงานร่วมแบบเปิดกว้าง(eXceed Camp)<br /><br />
+              <span>2563</span> &nbsp;&nbsp;คอร์สเรียนออนไลน์ udemy หัวข้อ Web Development Introduction<br /><br />
+              <span>2563</span> &nbsp;&nbsp;คอร์สเรียนออนไลน์ udemy หัวข้อ React JS - A Complete Guide for Frontend Web Development<br /><br />
+              <span>2563</span> &nbsp;&nbsp;ออกแบบ UX/UI ในรายวิชา  Software Engineering
+            </p>
+          </Col>
+        </Row>
+        <Row id={classes.skill}>
+          <Col>
+            <h1 className={classes.skillTitle}>Skill</h1>
+            <p className={classes.skillDetails}>
+              <br />
+              - Python<br /><br />
+              - UX/UI design(figma , framer)<br /><br />
+              - html<br /><br />
+              - Javascript<br /><br />
+              - ReactJS<br /><br />
+            </p>
+          </Col>
+          <Col>
+            <p className={classes.skillDetails2}>
+              <br />
+              - CSS/SASS<br /><br />
+              - Gulp<br /><br />
+              - English<br /><br />
+              - Bootstraps/React Bootstraps<br /><br />
+              - Git<br /><br />
+            </p>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
